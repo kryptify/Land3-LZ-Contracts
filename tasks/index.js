41545a3d3f6d567389f3ae3fd14e94be0232c02c
Kryptify <xinzhe1219@outlook.com>
2022-05-03 13:42:54 -0400

    Update nftMint.js

--- tasks/index.js
+++ tasks/index.js
@@ -6,7 +6,7 @@ task(
 ).addParam("targetNetwork", "the target network to let this instance receive messages from")
 
 // mint NFT on ETH
-task("nftMint", "mint() mint ExampleNFT", require("./nftMint"))
+task("nftMint", "mint ExampleNFT", require("./nftMint"))
 
 // set Trusted Remote for Avax contract
 task(
--- tasks/nftMint.js
+++ tasks/nftMint.js
@@ -1,10 +1,12 @@
 module.exports = async function (taskArgs, hre) {
+    const signers = await ethers.getSigners()
+    const owner = signers[0]
     const exampleNFT = await ethers.getContract("ExampleNFT")
     console.log(`[source] exampleNFT.address: ${exampleNFT.address}`)
 
     try {
-        let tx = await (await exampleNFT.mint()).wait()
-        console.log(`✅ [${hre.network.name}] mint()`)
+        let tx = await (await exampleNFT.mint(owner.address, 1)).wait()
+        console.log(`✅ [${hre.network.name}] mint`)
         console.log(` tx: ${tx.transactionHash}`)
         let nftTokenId = await ethers.provider.getTransactionReceipt(tx.transactionHash)
         console.log(` NFT nftId: ${parseInt(Number(nftTokenId.logs[0].topics[3]))}`)
