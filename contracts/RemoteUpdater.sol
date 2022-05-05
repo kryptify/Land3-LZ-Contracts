4fb38fcd1a91b842e97b024b804f582dd23ccf90
Kryptify <xinzhe1219@outlook.com>
2022-05-05 06:49:01 -0400

    Add Security to RemoteUpdater
    Remove Reentrancy Guard

--- contracts/RemoteUpdater.sol
+++ contracts/RemoteUpdater.sol
@@ -4,11 +4,10 @@ pragma solidity 0.8.4;
 pragma abicoder v2;
 
 import "./lzApp/NonblockingLzApp.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 /// @title A LayerZero example sending a cross chain message from a source chain to a destination chain to increment a counter
-contract RemoteUpdater is NonblockingLzApp, ReentrancyGuard, Pausable {
+contract RemoteUpdater is NonblockingLzApp, Pausable {
 
     constructor(address _lzEndpoint) NonblockingLzApp(_lzEndpoint) {}
 
@@ -24,7 +23,7 @@ contract RemoteUpdater is NonblockingLzApp, ReentrancyGuard, Pausable {
     function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory) internal override {}
 
     // update owner of NFT
-    function updateOwnerOfNFT(uint16 _dstChainId, uint _tokenId, address _newOwner) public payable nonReentrant whenNotPaused {
+    function updateOwnerOfNFT(uint16 _dstChainId, uint _tokenId, address _newOwner) public payable whenNotPaused {
         // encode the payload with the _tokenId and _newOwner
         bytes memory payload = abi.encode(_tokenId, msg.sender, _newOwner);
 
